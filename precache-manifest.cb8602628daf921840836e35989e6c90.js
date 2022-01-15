2481bbbf7d04020c2b9c48c77eba9e41899d3b82
amirvalizadeh <amirvalizadeh8731@gmail.com>
2022-01-16 00:10:38 +0330

    Updates

---
+++ precache-manifest.cb8602628daf921840836e35989e6c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e26e305996326de45a",
+    "url": "/twitter/static/css/main.96c90744.chunk.css"
+  },
+  {
+    "revision": "88e26e305996326de45a",
+    "url": "/twitter/static/js/main.ecb47282.chunk.js"
+  },
+  {
+    "revision": "8457224d8bc9a199591f",
+    "url": "/twitter/static/js/runtime~main.99fe10d7.js"
+  },
+  {
+    "revision": "690203ed2b873c42147f",
+    "url": "/twitter/static/css/2.0e2e9d9f.chunk.css"
+  },
+  {
+    "revision": "690203ed2b873c42147f",
+    "url": "/twitter/static/js/2.95bde589.chunk.js"
+  },
+  {
+    "revision": "ac01412d403666b67c14489863fc20a5",
+    "url": "/twitter/index.html"
+  }
+];
